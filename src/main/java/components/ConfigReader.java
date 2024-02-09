4fd57d7cccc6a00bbb959ed69312694107e43cb4
mecronorm <alessandropaap@hotmail.com>
2024-02-09 14:44:43 +0100

    Create hello world with Spring

---
+++ src/main/java/components/ConfigReader.java
@@ -0,0 +1,40 @@
+package components;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+@PropertySource("classpath:application.properties")
+public class ConfigReader {
+    @Value("${configured_language}")
+    private String configuredLanguage;
+
+    @Value("${ENG_hello_world}")
+    private String englishHelloWorld;
+
+    @Value("${NL_hello_world}")
+    private String dutchHelloWorld;
+
+    @Value("${FR_hello_world}")
+    private String frenchHelloWorld;
+
+    @Value("${GER_hello_world}")
+    private String germanHelloWorld;
+
+    public String getHelloWorldMessage() {
+        switch (configuredLanguage) {
+            case "ENG":
+                return englishHelloWorld;
+            case "NL":
+                return dutchHelloWorld;
+            case "FR":
+                return frenchHelloWorld;
+            case "GER":
+                return germanHelloWorld;
+            default:
+                return "Hello, World!";
+        }
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import components.ConfigReader;
+import config.ApplicationConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+        ConfigReader configReader = context.getBean(ConfigReader.class);
+
+        String message = configReader.getHelloWorldMessage();
+        System.out.println(message);
+    }
+}
